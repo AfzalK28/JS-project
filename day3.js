14fb49488407aa2a266cb9001fb223e521a12de8
AfzalK28 <afzalk280306@gmail.com>
2024-04-07 17:44:38 +0530

    JS essentials

---
+++ day3.js
@@ -0,0 +1,67 @@
+/* let statusVal = 2;
+
+switch(statusVal)
+{
+    case 1:
+        {
+            console.log("Super User");
+            break;
+        }
+    case 2:
+            {
+                console.log("Medium User");
+                break;
+            }
+    case 3:
+        {
+            console.log("Normal User");
+            break;
+        }
+    default:
+        {
+            console.log("Lower level user")
+        }
+} */
+
+/* let age = 15;
+
+let voteStatus = age>=18 ?  1 : 2;
+
+console.log(voteStatus);
+*/
+
+/*let a = 5;
+
+while(a<10)
+{
+console.log("Thank you world!");
+}*/
+
+//array
+
+/*let city = ["Mumbai","Pune","Hyderabad"];
+ for(let a=0; a<3; a++)
+ {
+    console.log(city[a]);
+ }*/
+
+
+ //objects
+
+ let student1=
+ {
+    name:"Afzal",
+    age:18,
+    city:"Mumbai"
+ }
+
+ let student2=
+ {
+    name:"Shrikant",
+    age:17,
+    city:"Pune"
+ }
+
+ console.log(student1);
+
+ 
